b1e3c5c2990410486ed9ad12a801ce01b14e17f5
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-11-13 12:01:33 +0530

    ddd

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -6,9 +6,12 @@ import RegistrationInfo from './RegistrationInfo.tsx'
 import { Provider } from 'react-redux'
 import store from './store/store.ts'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+    <ToastContainer />
     <RegistrationInfo />
     </Provider>
   </StrictMode>,
--- vite-project/src/saga/registrationsaga.ts
+++ vite-project/src/saga/registrationsaga.ts
@@ -2,7 +2,7 @@ import { call, takeEvery } from 'redux-saga/effects'
 import { registrationRequest } from '../slice/registrationslice'
 import registrationData from '../service/registration'
 import { PayloadAction } from '@reduxjs/toolkit'
-
+import {  toast } from 'react-toastify';
 
 
  function* watcherRegistration( action: PayloadAction<IRegistration>)
@@ -10,10 +10,14 @@ import { PayloadAction } from '@reduxjs/toolkit'
     try
     {
         const data =yield call(registrationData,action.payload)
-
+        console.log(data);
+        if(data.status !=200 || data.status !=201)
+        {
+            toast("error");
+        }
     }
     catch(error)
-    {
+    { toast("error");
 
     }
 
